7b57e43aac739bbe47e6598c9d36662ca7c29f81
janakiram <janakiram.gollapudi@gmail.com>
2019-12-01 13:09:09 -0500

    Updated console message

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const AircraftModel = require('../models/aircraft');
+
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
@@ -20,9 +21,8 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 const Aircraft = AircraftModel(sequelize, Sequelize);
-//{ force: true }
 sequelize.sync().then(() => {
-  console.log('Tables are created!');
+  console.log('Table created!');
 });
 
 module.exports = {
